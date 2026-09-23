29974a1ac687bad9d80ab9acd1f0195d41dba4b5
Logien Lasheen <logien.lasheen@student.giu-uni.de>
2026-09-23 22:33:35 +0300

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,9 @@
+let heroName = "logien lasheen";
+let studentID = 16005380;
+let power = studentID * 9;
+let warcry = "By Alexandria, the Sphinx Wraith kneels!";
+
+console.log(heroName);
+console.log(studentID);
+console.log(power);
+console.log(warcry);
